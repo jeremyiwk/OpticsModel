913285479a62519b9bc2d5f7d6b1184c55905ee3
jeremyiwk <jeremy@micromelody.net>
2023-12-11 16:28:53 -0700

    Added OpticsModel/src/ and simulation header files. sstarted column element class definition

---
+++ OpticsModel/src/column_element.h
@@ -0,0 +1,87 @@
+#include <Eigen/Dense>
+
+class ColumnElement
+{
+
+private:
+
+public:
+
+};
+
+class Aperture: public ColumnElement
+{
+
+private:
+
+public:
+
+	double z_min;
+	double z_max;
+	double r_minor;
+	double r_major;
+
+};
+
+
+class EinzelLens: public ColumnElement
+{
+
+private:
+
+public:
+
+	double z_min;
+	double z_max;
+	double radius;
+
+	double electrode1_length;
+	double insulator1_length;
+	double electrode2_length;
+	double insulator2_length;
+	double electrode3_length;
+
+	double electrode1_voltage = 0;
+	double electrode2_voltage = 1;
+	double electrode3_voltage = 0;
+
+	int element_z_resolution;
+	int element_t_resolution;
+
+	int charge_z_resolution;
+	int charge_t_resolution;
+	int charge_r_resolution;
+
+	double charge_z_min;
+	double charge_t_min;
+	double charge_r_min;
+	double charge_z_max;
+	double charge_t_max;
+	double charge_r_max;
+
+	Eigen::VectorXd element_x_coordinates;
+	Eigen::VectorXd element_y_coordinates;
+	Eigen::VectorXd element_z_coordinates;
+	Eigen::VectorXd element_voltage;
+
+	Eigen::VectorXd charge_x_coordinates;
+	Eigen::VectorXd charge_y_coordinates;
+	Eigen::VectorXd charge_z_coordinates;
+	Eigen::VectorXd charge_density;
+
+
+	EinzelLens()
+	{
+		std::cout << "initializing lens" << std::endl;
+	}
+
+};
+
+class Multipole: public ColumnElement
+{
+
+private:
+
+public:
+
+};
---
+++ OpticsModel/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <Eigen/Dense>
+#include "column_element.h"
+#include "integrator.h"
+#include "simulation.h"
+
+int main()
+{
+
+	EinzelLens lens1;
+
+	std::cout << "code ran successfully" << std::endl;
+
+	return 0;
+}
